f7479e3446ea44e246aea8db9a115076cd8076f9
Earl Jeno Garcia <68950014+earlhsjks@users.noreply.github.com>
2025-03-26 12:15:30 +0800

    to port 5001

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ def initialize_database():
 # Run Flask App
 if __name__ == '__main__':
     logging.info("Starting Flask App...")
-    serve(app, host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=5001)
     # app.run(host='0.0.0.0', port=80, debug=True)
